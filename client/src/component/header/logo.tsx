90ca46792fd88ac1035e3e1115adc753148a0e0c
cx9s <cx9918@gmail.com>
2023-03-11 20:23:32 +0000

    add logo, ico and style

--- client/src/component/header/logo.tsx
+++ client/src/component/header/logo.tsx
@@ -1,10 +1,10 @@
-import logo from "../../assets/bg.jpg";
+import logo from "../../assets/logo.jpg";
 import { Link } from "react-router-dom";
 
 const Logo: React.FC = () => {
   return (
     <Link to={`/`} className="logo">
-      <img src={logo} width="60" height="45" />
+      <img src={logo} width="60" height="55" />
     </Link>
   );
 };
