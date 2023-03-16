134a8391dea949f3b1c7d873e4742d82477c9fdb
cx9s <cx9918@gmail.com>
2023-03-16 14:49:32 +0000

    delete app.use(cors())

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,24 +10,24 @@ const PORT = SERVER_PORT;
 console.log(`🌍 Running in ${environment} environment`);
 
 try {
-	app.use(cors());
-	
-	// todo for now this creates the DB new each time server is run
-	/*if (environment === "dev")*/ {
-		//databaseInit();
-	}
+  // todo for now this creates the DB new each time server is run
+  /*if (environment === "dev")*/ {
+    //databaseInit();
+  }
 
-	const server = app.listen(PORT, () => {
-		if(LOG_SERVER_OUTPUT) console.log(`Server is now listening on port: ${PORT}`);
-	})
-	.on("error", (error) => {
-		console.error("Express Error");
-		console.error(error);
-	});
+  const server = app
+    .listen(PORT, () => {
+      if (LOG_SERVER_OUTPUT)
+        console.log(`Server is now listening on port: ${PORT}`);
+    })
+    .on("error", (error) => {
+      console.error("Express Error");
+      console.error(error);
+    });
 
-	process.on("SIGINT", () => handleShutdown(server));
-	process.on("SIGTERM", () => handleShutdown(server));
-	process.on("SIGHUP", () => handleShutdown(server));
+  process.on("SIGINT", () => handleShutdown(server));
+  process.on("SIGTERM", () => handleShutdown(server));
+  process.on("SIGHUP", () => handleShutdown(server));
 
   process.on("SIGINT", () => handleShutdown(server));
   process.on("SIGTERM", () => handleShutdown(server));
