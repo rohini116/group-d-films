65bb3a08047831d5b5df02cc6d43270d0d35a2cf
skip-rat <skip-rat@users.noreply.github.com>
2023-03-16 14:50:10 +0000

    Updated readme
    Included issue with dotenv on mac

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,15 @@
 import express from "express";
 import { initialiseRoutes, router } from "./routes/routes";
-import cors from "cors";
 
 export const app = express();
 
 // read server cmd line args
 // looking for arg to enable/disable logging server output, so it can be disabled when running tests
 const arr = process.argv.slice(2);
-export const LOG_SERVER_OUTPUT =
-  arr.find((arg) => arg === "log-server-output") !== undefined;
+export const LOG_SERVER_OUTPUT = arr.find(arg => arg === 'log-server-output') !== undefined;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 initialiseRoutes(app);
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ const environment = getEnv();
 import { app, LOG_SERVER_OUTPUT } from "./app";
 //import { databaseInit } from "./database/database_seed";
 import { Server } from "http";
+import cors from "cors";
+
 
 const PORT = SERVER_PORT;
 
@@ -29,16 +31,13 @@ try {
 	process.on("SIGTERM", () => handleShutdown(server));
 	process.on("SIGHUP", () => handleShutdown(server));
 
-  process.on("SIGINT", () => handleShutdown(server));
-  process.on("SIGTERM", () => handleShutdown(server));
-  process.on("SIGHUP", () => handleShutdown(server));
 } catch (e: unknown) {
-  console.error("🚨 Top level Error caught 🚨 ");
-  console.error((e as Error).message);
+	console.error("🚨 Top level Error caught 🚨 ");
+	console.error((e as Error).message);
 }
 
 function handleShutdown(server: Server) {
-  server.close(() => {
-    process.exit(0);
-  });
+	server.close(() => {
+		process.exit(0);
+	});
 }
