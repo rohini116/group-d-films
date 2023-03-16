3f337a3b19cc5fe689a57e0579abfc242b1a7756
cx9s <cx9918@gmail.com>
2023-03-16 00:19:10 +0000

    fix CORS issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,40 +4,37 @@ const environment = getEnv();
 import { app, LOG_SERVER_OUTPUT } from "./app";
 //import { databaseInit } from "./database/database_seed";
 import { Server } from "http";
-import cors from "cors";
-
 
 const PORT = SERVER_PORT;
 
 console.log(`🌍 Running in ${environment} environment`);
 
 try {
-	app.use(cors());
+  // todo for now this creates the DB new each time server is run
+  /*if (environment === "dev")*/ {
+    //databaseInit();
+  }
 
-	// todo for now this creates the DB new each time server is run
-	/*if (environment === "dev")*/ {
-		//databaseInit();
-	}
+  const server = app
+    .listen(PORT, () => {
+      if (LOG_SERVER_OUTPUT)
+        console.log(`Server is now listening on port: ${PORT}`);
+    })
+    .on("error", (error) => {
+      console.error("Express Error");
+      console.error(error);
+    });
 
-	const server = app.listen(PORT, () => {
-		if(LOG_SERVER_OUTPUT) console.log(`Server is now listening on port: ${PORT}`);
-	})
-	.on("error", (error) => {
-		console.error("Express Error");
-		console.error(error);
-	});
-
-	process.on("SIGINT", () => handleShutdown(server));
-	process.on("SIGTERM", () => handleShutdown(server));
-	process.on("SIGHUP", () => handleShutdown(server));
-
+  process.on("SIGINT", () => handleShutdown(server));
+  process.on("SIGTERM", () => handleShutdown(server));
+  process.on("SIGHUP", () => handleShutdown(server));
 } catch (e: unknown) {
-	console.error("🚨 Top level Error caught 🚨 ");
-	console.error((e as Error).message);
+  console.error("🚨 Top level Error caught 🚨 ");
+  console.error((e as Error).message);
 }
 
 function handleShutdown(server: Server) {
-	server.close(() => {
-		process.exit(0);
-	});
+  server.close(() => {
+    process.exit(0);
+  });
 }
