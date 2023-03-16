46c6f76d474fde8d1b04fd98a04128a1899ac1e7
cx9s <cx9918@gmail.com>
2023-03-16 00:40:39 +0000

    construct movie card by movie type

--- client/src/component/home/movieCard.tsx
+++ client/src/component/home/movieCard.tsx
@@ -1,17 +1,19 @@
 import { Movie } from "../../type/movie.types";
+import { BASE_URL, POSTER_SIZE } from "../../configs/moviesImg";
 
 export interface MovieCardProps {
   movie: Movie;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
-  const { title, date, imgUrl } = movie;
+  const { title, release_date, poster_path } = movie;
+  const imgUrl = BASE_URL + POSTER_SIZE + poster_path;
 
   return (
     <div className="movieCard">
       <h3>Title: {title}</h3>
-      <p>Release date: {date}</p>
-      <img title={title} src={imgUrl} width="200" height="200" />
+      <p>Release date: {release_date}</p>
+      <img title={title} src={imgUrl} />
     </div>
   );
 };
