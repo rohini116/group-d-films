29f997704a5d64584bd22b8b9f6168211e83086a
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-03-19 13:09:44 +0000

    style: add basic About styling

--- client/src/component/about/about.tsx
+++ client/src/component/about/about.tsx
@@ -1,14 +1,16 @@
 const About: React.FC = () => {
   return (
-    <div className="background-img">
-      <h2 className="title-style--indigo">Group D Films Team Members</h2>
-      <ul>
-        <li>Lloyd</li>
-        <li>Michelle</li>
-        <li>Rohini</li>
-        <li>Ryan</li>
-        <li>Xuan</li>
-      </ul>
+    <div className='background-gradient'>
+      <h2 className='about-container title-style'>Group D Films Team</h2>
+        <div>
+          <ul className='about-container__list title-style'>
+            <li className='about-list__items'>Lloyd</li>
+            <li className='about-list__items'>Michelle</li>
+            <li className='about-list__items'>Rohini</li>
+            <li className='about-list__items'>Ryan</li>
+            <li className='about-list__items'>Xuan</li>
+          </ul>
+        </div>
     </div>
   );
 };
